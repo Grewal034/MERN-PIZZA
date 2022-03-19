14b0d5de70a957cbd824c851683cbd00de116277
Ricky Grewal <Rickygrewal@protonmail.com>
2022-03-19 00:15:10 -0600

    price updates with quanity

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Pizza({pizza}) {
+    const [ quantity , setQuantity ] = useState(1);
+    const [ varient , setvarient ] = useState('small');
   return (
     <div>
         <h1>{pizza.name}</h1>
@@ -9,7 +11,7 @@ export default function Pizza({pizza}) {
         <div className="flex-container">
             <div className='w-100'>
                 <p>Varients</p>
-                <select>
+                <select value = {varient} onChange={(e)=>{setvarient(e.target.value)}}>
                 {pizza.varients.map(varient =>{
                     return <option>{varient}</option>
                 } )}
@@ -17,13 +19,22 @@ export default function Pizza({pizza}) {
             </div>
             <div className='w-100'>
                 <p>Quantity</p>
-                <select>
+                <select value = {quantity} onChange={(e)=>{setvarient(e.target.value)}}>
                    {[...Array(10).keys()].map((x, i)=>{
                      return <option value = {i+1}>{i+1}</option>  // creating 10 quantities for each pizza
                    })}
                 </select>
             </div>
             
+        </div>
+
+        <div className="flex-container">
+            <div>
+                   <h1>Price: {pizza.prices[0][varient] * quantity}</h1>
+            </div>
+            <div>
+                   <button className="btn btn-primary">Add to Cart</button>
+            </div>
         </div>
     </div>
   )
