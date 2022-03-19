67931acef11411cd694a1b72a667dfce10c4866c
Ricky Grewal <Rickygrewal@protonmail.com>
2022-03-18 23:57:30 -0600

    created quanity

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -16,7 +16,12 @@ export default function Pizza({pizza}) {
                 </select>
             </div>
             <div className='w-100'>
-                <p>Prices</p>
+                <p>Quantity</p>
+                <select>
+                   {[...Array(10).keys()].map((x, i)=>{
+                     return <option value = {i+1}>{i+1}</option>  // creating 10 quantities for each pizza
+                   })}
+                </select>
             </div>
             
         </div>
