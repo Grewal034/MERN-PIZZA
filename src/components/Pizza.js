e8c4ffdda83b31bd837d55ef96164fa65c13c9df
Ricky Grewal <Rickygrewal@protonmail.com>
2022-03-19 00:38:50 -0600

    Finished pizza component

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -4,22 +4,22 @@ export default function Pizza({pizza}) {
     const [ quantity , setQuantity ] = useState(1);
     const [ varient , setvarient ] = useState('small');
   return (
-    <div>
+    <div style={{margin:'70px'}} className='shadow-lg p-3 mb-5 bg-white rounded'>
         <h1>{pizza.name}</h1>
         <img src={pizza.image} className="img-fluid" style ={{height: '200px' , width:'200px'} }/>
 
         <div className="flex-container">
-            <div className='w-100'>
+            <div className='w-100 m-1'>
                 <p>Varients</p>
-                <select value = {varient} onChange={(e)=>{setvarient(e.target.value)}}>
+                <select className='form-control' value = {varient} onChange={(e)=>{setvarient(e.target.value)}}>
                 {pizza.varients.map(varient =>{
                     return <option>{varient}</option>
                 } )}
                 </select>
             </div>
-            <div className='w-100'>
+            <div className='w-100 m-1' >
                 <p>Quantity</p>
-                <select value = {quantity} onChange={(e)=>{setvarient(e.target.value)}}>
+                <select className='form-control' value = {quantity} onChange={(e)=>{setvarient(e.target.value)}}>
                    {[...Array(10).keys()].map((x, i)=>{
                      return <option value = {i+1}>{i+1}</option>  // creating 10 quantities for each pizza
                    })}
@@ -29,10 +29,10 @@ export default function Pizza({pizza}) {
         </div>
 
         <div className="flex-container">
-            <div>
-                   <h1>Price: {pizza.prices[0][varient] * quantity}</h1>
+            <div className='m-1 w100'>
+                   <h1 className='mt-1'>Price: {pizza.prices[0][varient] * quantity} Rs/</h1>
             </div>
-            <div>
+            <div className='m-1 w100'>
                    <button className="btn btn-primary">Add to Cart</button>
             </div>
         </div>
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -8,7 +8,7 @@ export default function Homepage() {
         <div className="row">
             {pizzas.map(pizza =>{
                 return <div className="col-md-4">
-                    <div>
+                    <div >
                         <Pizza pizza={pizza}/>    
                     </div>
                 </div>
